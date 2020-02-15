531a83c52b762fb1bef6fc4cc6b9c84fa976bae7
M1st1f <57596595+M1st1f@users.noreply.github.com>
2020-02-14 23:27:05 -0500

    A new update
    I just add a 3 function. I pretend to add a 4 near soon.
    Goodbye and see you, M1st1f
    <(o)>

--- OS.py
+++ OS.py
@@ -39,6 +39,47 @@ command = input(">>> ")
 # Call program calculator.py
 #
 if command == "1":
+    #
+# user information(working on)
+#
+emanresu = ("Still")
+drowssap = ("a-test")
+
+#
+# Title of program
+#
+print(" ==========∅========= ")
+print("   /$$$$$$   /$$$$$$  ")
+print("  /$$__  $$ /$$__  $$ ")
+print(" | $$  \ $$| $$  \__/ ")
+print(" | $$  | $$|  $$$$$$  ")
+print(" | $$  | $$ \____  $$ ")
+print(" | $$  | $$ /$$  \ $$ ")
+print(" |  $$$$$$/|  $$$$$$  ")
+print("  \______/  \______/  ")
+print(" ==========∅========= ")
+
+#
+# start of verifiction
+#
+print("Please enter your user")
+
+username = input("user: ")
+password = input("password: ")
+
+if username == emanresu and password == drowssap:
+    print("welcome", username)
+
+print("what you want to do?")
+print("1.calculator")
+print("2.text writer")
+print("3.exit")
+command = input(">>> ")
+
+#
+# Call program calculator.py
+#
+if command == "1":
     print("========================================================================================")
     print("                     /$$                     /$$             /$$                        ")
     print("                    | $$                    | $$            | $$                        ")
@@ -56,11 +97,20 @@ if command == "1":
 
 
 #
-# Working on
+# Working on (not so good at this part)
 #
 if command == "2":
-    print("dd")
-
+    print("==================================================")
+    print(" /$$$$$$$  /$$$$$$  /$$$$$$  /$$$$$$$  /$$     /$$")
+    print("| $$__  $$|_  $$_/ /$$__  $$| $$__  $$|  $$   /$$/")
+    print("| $$  \ $$  | $$  | $$  \ $$| $$  \ $$ \  $$ /$$/ ")
+    print("| $$  | $$  | $$  | $$$$$$$$| $$$$$$$/  \  $$$$/  ")
+    print("| $$  | $$  | $$  | $$__  $$| $$__  $$   \  $$/   ")
+    print("| $$  | $$  | $$  | $$  | $$| $$  \ $$    | $$    ")
+    print("| $$$$$$$/ /$$$$$$| $$  | $$| $$  | $$    | $$    ")
+    print("|_______/ |______/|__/  |__/|__/  |__/    |__/    ")
+    print("==================================================")
+    exec(open("text writer.py").read())
 #
 # program to exit the system
 #
