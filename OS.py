cdd2b409319e80a93e5094758c466290a39e86ba
M1st1f <57596595+M1st1f@users.noreply.github.com>
2020-03-23 17:14:00 -0300

    Update OS.py

--- OS.py
+++ OS.py
@@ -29,51 +29,12 @@ password = input("password: ")
 if username == emanresu and password == drowssap:
     print("welcome", username)
 
-print("what you want to do?")
-print("1.calculator")
-print("2.text writer")
-print("3.exit")
-command = input(">>> ")
-
-#
-# Call program calculator.py
-#
-if command == "1":
-    #
-# user information(working on)
-#
-emanresu = ("Still")
-drowssap = ("a-test")
-
-#
-# Title of program
-#
-print(" ==========∅========= ")
-print("   /$$$$$$   /$$$$$$  ")
-print("  /$$__  $$ /$$__  $$ ")
-print(" | $$  \ $$| $$  \__/ ")
-print(" | $$  | $$|  $$$$$$  ")
-print(" | $$  | $$ \____  $$ ")
-print(" | $$  | $$ /$$  \ $$ ")
-print(" |  $$$$$$/|  $$$$$$  ")
-print("  \______/  \______/  ")
-print(" ==========∅========= ")
-
-#
-# start of verifiction
-#
-print("Please enter your user")
-
-username = input("user: ")
-password = input("password: ")
-
-if username == emanresu and password == drowssap:
-    print("welcome", username)
 
 print("what you want to do?")
 print("1.calculator")
 print("2.text writer")
-print("3.exit")
+print("3.webbrowser")
+print("4.exit")
 command = input(">>> ")
 
 #
@@ -97,7 +58,7 @@ if command == "1":
 
 
 #
-# Working on (not so good at this part)
+# Working on
 #
 if command == "2":
     print("==================================================")
@@ -111,8 +72,25 @@ if command == "2":
     print("|_______/ |______/|__/  |__/|__/  |__/    |__/    ")
     print("==================================================")
     exec(open("text writer.py").read())
+
+
+if command == "3":
+    print("=================================")
+    print(" /$$      /$$           /$$      ")
+    print("| $$  /$ | $$          | $$      ")
+    print("| $$ /$$$| $$  /$$$$$$ | $$$$$$$ ")
+    print("| $$/$$ $$ $$ /$$__  $$| $$__  $$")
+    print("| $$$$_  $$$$| $$$$$$$$| $$  \ $$")
+    print("| $$$/ \  $$$| $$_____/| $$  | $$")
+    print("| $$/   \  $$|  $$$$$$$| $$$$$$$/")
+    print("|__/     \__/ \_______/|_______/ ")
+    print("=================================")
+    exec(open("algo interessante.py").read())
 #
 # program to exit the system
 #
-if command == "3":
+if command == "4":
     SystemExit
+
+if command >= "5":
+    print("Invalid input")
